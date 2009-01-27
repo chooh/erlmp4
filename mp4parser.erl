e1c9bb222cdd20535fa61c2f763c42dc4e8b565f
Sergey Litovchenko <chooh@chooh.msk.ru>
2009-01-27 04:37:33 +0300

    Use lists instead of dict, found second trak atom :)

--- mp4parser.erl
+++ mp4parser.erl
@@ -5,45 +5,65 @@
 
 -record(mp4_atom, {
     type,
-    atom_size,
+    size_base,
+    size_ext=0,
     start}).
 
--record mvhd_atom, {
-    version,
-    flags,
-    creation_time,
-    modification_time_,
-    timescale,
-    duration,
-    rate,
-    volume,
-    reserved1,
-    reserved2,
-    matrix,
-    predefined,
-    next_track_id}).
+%-record(mvhd_atom, {
+%    version,
+%    flags,
+%    creation_time,
+%    modification_time_,
+%    timescale,
+%    duration,
+%    rate,
+%    volume,
+%    reserved1,
+%    reserved2,
+%    matrix,
+%    predefined,
+%    next_track_id}).
 
-atom_read_header(File, Pos, Atoms) ->
-    case file:pread(File, Pos, 8) of
+read_atoms(File) ->
+    read_atoms(File, 0, 0, []).
+
+read_atoms(File, Pos, End) ->
+    read_atoms(File, Pos, End, []).
+
+read_atoms(File, Pos, End, Atoms) when (End > Pos) or (End == 0) ->
+    case file:pread(File, Pos, ?ATOM_PREAMBLE_SIZE) of
         {ok, Bin} ->
             {<<Size:32>>, Type} = split_binary(Bin, 4),
-%            io:format("Atom [~s], length ~w~n", [Type, Size]),
-            NewAtoms = dict:store(binary_to_list(Type), #mp4_atom{type=Type, atom_size=Size, start=Pos}, Atoms),
-            atom_read_header(File, Pos + Size, NewAtoms);
+            % TODO: Check if SizeExt is needed
+            NewAtoms = lists:append(Atoms, [#mp4_atom{type=binary_to_list(Type), size_base=Size, start=Pos}]),
+            read_atoms(File, Pos + Size, End, NewAtoms);
         eof ->
             Atoms
-    end.
+    end;
 
+read_atoms(_File, _Pos, _End, Atoms) ->
+    Atoms.
+
+find_atom(Type, Atoms) ->
+    case lists:filter(fun(X) -> X#mp4_atom.type == Type end, Atoms) of
+        [H|_] ->
+            H;
+        [] ->
+            {error, not_found}
+     end.
+
+pp_atoms(Atoms) ->
+    lists:foreach(fun(X) -> io:format("Atom [~s], Start ~w, Length ~w~n", [
+        X#mp4_atom.type, X#mp4_atom.start, X#mp4_atom.size_base]) end, Atoms).
 
 main(FileName, _StartTime) ->
     {ok, File} = file:open(FileName, [read,binary,raw]),
-    RootAtoms = atom_read_header(File, 0, dict:new()),
+
+    RootAtoms = read_atoms(File),
     io:format("Root atoms:~n", []),
-    lists:map(fun(Key) -> X = dict:fetch(Key, RootAtoms), io:format("Atom [~s], Start ~w, Length ~w~n", [
-        X#mp4_atom.type, X#mp4_atom.start, X#mp4_atom.atom_size]) end, dict:fetch_keys(RootAtoms)),
-    MoovAtom = dict:fetch("moov", RootAtoms),
-    MoovAtoms = atom_read_header(File, MoovAtom#mp4_atom.start + ?ATOM_PREAMBLE_SIZE, dict:new()),
+    pp_atoms(RootAtoms),
+    MoovAtom = find_atom("moov", RootAtoms),
+    MoovAtoms = read_atoms(File, MoovAtom#mp4_atom.start + ?ATOM_PREAMBLE_SIZE, MoovAtom#mp4_atom.start + MoovAtom#mp4_atom.size_base),
     io:format("Moov atoms:~n", []),
-    lists:map(fun(Key) -> X = dict:fetch(Key, MoovAtoms), io:format("Atom [~s], Start ~w, Length ~w~n", [
-        X#mp4_atom.type, X#mp4_atom.start, X#mp4_atom.atom_size]) end, dict:fetch_keys(MoovAtoms)),
+    pp_atoms(MoovAtoms),
     file:close(File).
